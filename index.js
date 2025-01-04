e43035d52aa3be820d3add726cb8f47a129e3fda
Sohelrana2815 <sohel152302@gmail.com>
2025-01-04 21:18:56 +0600

    search field api

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use(
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5q2fm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5q2fm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-const uri = "mongodb://localhost:27017";
+// const uri = "mongodb://localhost:27017";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -221,6 +221,13 @@ async function run() {
       }
 
       res.send({ message: "View count updated successfully" });
+    });
+
+    // search articles by title
+
+    app.get("/searchArticles", async (req, res) => {
+      const searchTerm = req.query.title;
+      console.log(searchTerm);
     });
 
     // Get 6 trending articles by views in descending order
